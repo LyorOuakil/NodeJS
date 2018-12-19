230aa4872e5bf1df0a758d861b623a03157cd974
Lyor <lyor.ouakil@gmail.com>
2018-12-19 09:25:50 +0100

    Add exercice3, modif -> check if file exists

--- ex_03/ex_03.js
+++ ex_03/ex_03.js
@@ -2,8 +2,15 @@ var fs = require('fs');
 var arg1 = (process.argv[2]); // Take first argument
 var arg2 = process.argv[3]; // take second argument
 
-//Check if file exists : 
-     // Do code here
+// Check if file exists : 
+fs.stat('./'+arg1+'.txt', function(err){
+    if(!err){
+        console.log("file exists");
+    }else{
+        console.log("Error:_Critical_Failure");
+        return false;
+    }
+});
 
 // Read file 
 fs.readFile( arg1+'.txt', 'utf8', function(err, data){
@@ -14,6 +21,6 @@ fs.readFile( arg1+'.txt', 'utf8', function(err, data){
             if(err) throw err;
         });
     }else{
-        throw err;
+        return false;
     }
 })
